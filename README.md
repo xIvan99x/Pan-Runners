bb9bac17de4f45db8a172292e3fc9a892d946e5a
xIvan99x <ximprovefuture9x@gmail.com>
2025-01-31 21:04:19 +1300

    music

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Si eres un pan con conciencia tambien puedes tener ojos.
 Llevas un tiempo planeando tu escape y ahora por fin es el momento de llevarlo a cabo. El objetivo es huir de tu propio creador, alguien en quien pensaste que podias confiar,
 pero no! Debes correr lejos de el cuanto antes o te venderan para ser comido! O aun peor! Seras botado a la basura para nunca mas ser visto. Asi que huye! Corre! Y sobre todo no mires atras,
 tu tamaño comparado al del panadero puede ser un poco intimidante.
+Cambie las direcciones de las musicas de fondo en el codigo.
--- obj/Debug/net8.0/Proyecto.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff6e3020a9b107be6ade88e7d252664e5a2d112")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcef1f7323b82b0288a16b34f81fc3b183173360")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
